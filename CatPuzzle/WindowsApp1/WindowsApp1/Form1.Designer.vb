fe32b283f263514453ae343fada0f6729fafdb8b
Tracy Karol <tmkarol@mines.edu>
2018-11-27 16:53:47 -0700

    Finished cat puzzle program

--- CatPuzzle/WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ CatPuzzle/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -32,14 +32,17 @@ Partial Class Form1
         Me.PB1 = New System.Windows.Forms.PictureBox()
         Me.PB3 = New System.Windows.Forms.PictureBox()
         Me.PB2 = New System.Windows.Forms.PictureBox()
+        Me.PB15 = New System.Windows.Forms.PictureBox()
         Me.PB14 = New System.Windows.Forms.PictureBox()
         Me.PB13 = New System.Windows.Forms.PictureBox()
-        Me.PB15 = New System.Windows.Forms.PictureBox()
         Me.PB10 = New System.Windows.Forms.PictureBox()
         Me.PB11 = New System.Windows.Forms.PictureBox()
         Me.PB8 = New System.Windows.Forms.PictureBox()
         Me.PB12 = New System.Windows.Forms.PictureBox()
         Me.PB9 = New System.Windows.Forms.PictureBox()
+        Me.blankBox = New System.Windows.Forms.PictureBox()
+        Me.congrats = New System.Windows.Forms.Label()
+        Me.tip = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB5, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -48,14 +51,15 @@ Partial Class Form1
         CType(Me.PB1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB15, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB14, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB13, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PB15, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB10, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB11, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB8, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB12, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PB9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.blankBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ResetButton
@@ -73,16 +77,16 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.PictureBox1.Location = New System.Drawing.Point(353, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(353, 16)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(447, 662)
+        Me.PictureBox1.Size = New System.Drawing.Size(431, 635)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
         'PB4
         '
         Me.PB4.Image = CType(resources.GetObject("PB4.Image"), System.Drawing.Image)
-        Me.PB4.Location = New System.Drawing.Point(688, 27)
+        Me.PB4.Location = New System.Drawing.Point(672, 27)
         Me.PB4.Name = "PB4"
         Me.PB4.Size = New System.Drawing.Size(103, 153)
         Me.PB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -92,7 +96,7 @@ Partial Class Form1
         'PB5
         '
         Me.PB5.Image = CType(resources.GetObject("PB5.Image"), System.Drawing.Image)
-        Me.PB5.Location = New System.Drawing.Point(363, 186)
+        Me.PB5.Location = New System.Drawing.Point(363, 180)
         Me.PB5.Name = "PB5"
         Me.PB5.Size = New System.Drawing.Size(103, 153)
         Me.PB5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -102,7 +106,7 @@ Partial Class Form1
         'PB6
         '
         Me.PB6.Image = CType(resources.GetObject("PB6.Image"), System.Drawing.Image)
-        Me.PB6.Location = New System.Drawing.Point(472, 186)
+        Me.PB6.Location = New System.Drawing.Point(466, 180)
         Me.PB6.Name = "PB6"
         Me.PB6.Size = New System.Drawing.Size(103, 153)
         Me.PB6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -112,7 +116,7 @@ Partial Class Form1
         'PB7
         '
         Me.PB7.Image = CType(resources.GetObject("PB7.Image"), System.Drawing.Image)
-        Me.PB7.Location = New System.Drawing.Point(579, 186)
+        Me.PB7.Location = New System.Drawing.Point(569, 180)
         Me.PB7.Name = "PB7"
         Me.PB7.Size = New System.Drawing.Size(103, 153)
         Me.PB7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -132,7 +136,7 @@ Partial Class Form1
         'PB3
         '
         Me.PB3.Image = CType(resources.GetObject("PB3.Image"), System.Drawing.Image)
-        Me.PB3.Location = New System.Drawing.Point(579, 27)
+        Me.PB3.Location = New System.Drawing.Point(569, 27)
         Me.PB3.Name = "PB3"
         Me.PB3.Size = New System.Drawing.Size(103, 153)
         Me.PB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -142,47 +146,47 @@ Partial Class Form1
         'PB2
         '
         Me.PB2.Image = CType(resources.GetObject("PB2.Image"), System.Drawing.Image)
-        Me.PB2.Location = New System.Drawing.Point(470, 27)
+        Me.PB2.Location = New System.Drawing.Point(466, 27)
         Me.PB2.Name = "PB2"
         Me.PB2.Size = New System.Drawing.Size(103, 153)
         Me.PB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PB2.TabIndex = 9
         Me.PB2.TabStop = False
         '
+        'PB15
+        '
+        Me.PB15.Image = CType(resources.GetObject("PB15.Image"), System.Drawing.Image)
+        Me.PB15.Location = New System.Drawing.Point(569, 486)
+        Me.PB15.Name = "PB15"
+        Me.PB15.Size = New System.Drawing.Size(103, 153)
+        Me.PB15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB15.TabIndex = 11
+        Me.PB15.TabStop = False
+        '
         'PB14
         '
         Me.PB14.Image = CType(resources.GetObject("PB14.Image"), System.Drawing.Image)
-        Me.PB14.Location = New System.Drawing.Point(581, 504)
+        Me.PB14.Location = New System.Drawing.Point(466, 486)
         Me.PB14.Name = "PB14"
         Me.PB14.Size = New System.Drawing.Size(103, 153)
         Me.PB14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PB14.TabIndex = 11
+        Me.PB14.TabIndex = 12
         Me.PB14.TabStop = False
         '
         'PB13
         '
         Me.PB13.Image = CType(resources.GetObject("PB13.Image"), System.Drawing.Image)
-        Me.PB13.Location = New System.Drawing.Point(472, 504)
+        Me.PB13.Location = New System.Drawing.Point(363, 486)
         Me.PB13.Name = "PB13"
         Me.PB13.Size = New System.Drawing.Size(103, 153)
         Me.PB13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PB13.TabIndex = 12
+        Me.PB13.TabIndex = 13
         Me.PB13.TabStop = False
-        '
-        'PB15
-        '
-        Me.PB15.Image = CType(resources.GetObject("PB15.Image"), System.Drawing.Image)
-        Me.PB15.Location = New System.Drawing.Point(688, 504)
-        Me.PB15.Name = "PB15"
-        Me.PB15.Size = New System.Drawing.Size(103, 153)
-        Me.PB15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PB15.TabIndex = 13
-        Me.PB15.TabStop = False
         '
         'PB10
         '
         Me.PB10.Image = CType(resources.GetObject("PB10.Image"), System.Drawing.Image)
-        Me.PB10.Location = New System.Drawing.Point(472, 345)
+        Me.PB10.Location = New System.Drawing.Point(466, 333)
         Me.PB10.Name = "PB10"
         Me.PB10.Size = New System.Drawing.Size(103, 153)
         Me.PB10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -192,7 +196,7 @@ Partial Class Form1
         'PB11
         '
         Me.PB11.Image = CType(resources.GetObject("PB11.Image"), System.Drawing.Image)
-        Me.PB11.Location = New System.Drawing.Point(579, 345)
+        Me.PB11.Location = New System.Drawing.Point(569, 333)
         Me.PB11.Name = "PB11"
         Me.PB11.Size = New System.Drawing.Size(103, 153)
         Me.PB11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -202,7 +206,7 @@ Partial Class Form1
         'PB8
         '
         Me.PB8.Image = CType(resources.GetObject("PB8.Image"), System.Drawing.Image)
-        Me.PB8.Location = New System.Drawing.Point(688, 186)
+        Me.PB8.Location = New System.Drawing.Point(672, 180)
         Me.PB8.Name = "PB8"
         Me.PB8.Size = New System.Drawing.Size(103, 153)
         Me.PB8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -212,7 +216,7 @@ Partial Class Form1
         'PB12
         '
         Me.PB12.Image = CType(resources.GetObject("PB12.Image"), System.Drawing.Image)
-        Me.PB12.Location = New System.Drawing.Point(688, 345)
+        Me.PB12.Location = New System.Drawing.Point(672, 333)
         Me.PB12.Name = "PB12"
         Me.PB12.Size = New System.Drawing.Size(103, 153)
         Me.PB12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -222,12 +226,46 @@ Partial Class Form1
         'PB9
         '
         Me.PB9.Image = CType(resources.GetObject("PB9.Image"), System.Drawing.Image)
-        Me.PB9.Location = New System.Drawing.Point(363, 345)
+        Me.PB9.Location = New System.Drawing.Point(363, 333)
         Me.PB9.Name = "PB9"
         Me.PB9.Size = New System.Drawing.Size(103, 153)
         Me.PB9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PB9.TabIndex = 18
         Me.PB9.TabStop = False
+        '
+        'blankBox
+        '
+        Me.blankBox.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.blankBox.Location = New System.Drawing.Point(672, 486)
+        Me.blankBox.Name = "blankBox"
+        Me.blankBox.Size = New System.Drawing.Size(103, 153)
+        Me.blankBox.TabIndex = 19
+        Me.blankBox.TabStop = False
+        '
+        'congrats
+        '
+        Me.congrats.AutoSize = True
+        Me.congrats.Font = New System.Drawing.Font("Poor Richard", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.congrats.ForeColor = System.Drawing.Color.Navy
+        Me.congrats.Location = New System.Drawing.Point(907, 198)
+        Me.congrats.Name = "congrats"
+        Me.congrats.Size = New System.Drawing.Size(138, 44)
+        Me.congrats.TabIndex = 20
+        Me.congrats.Text = "Nice job!"
+        Me.congrats.Visible = False
+        '
+        'tip
+        '
+        Me.tip.AutoSize = True
+        Me.tip.Font = New System.Drawing.Font("Poor Richard", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tip.ForeColor = System.Drawing.Color.Navy
+        Me.tip.Location = New System.Drawing.Point(817, 289)
+        Me.tip.Name = "tip"
+        Me.tip.Size = New System.Drawing.Size(359, 44)
+        Me.tip.TabIndex = 21
+        Me.tip.Text = "Press Reset to play again!"
+        Me.tip.UseWaitCursor = True
+        Me.tip.Visible = False
         '
         'Form1
         '
@@ -235,14 +273,17 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(1188, 795)
+        Me.Controls.Add(Me.tip)
+        Me.Controls.Add(Me.congrats)
+        Me.Controls.Add(Me.blankBox)
         Me.Controls.Add(Me.PB9)
         Me.Controls.Add(Me.PB12)
         Me.Controls.Add(Me.PB8)
         Me.Controls.Add(Me.PB11)
         Me.Controls.Add(Me.PB10)
-        Me.Controls.Add(Me.PB15)
         Me.Controls.Add(Me.PB13)
         Me.Controls.Add(Me.PB14)
+        Me.Controls.Add(Me.PB15)
         Me.Controls.Add(Me.PB2)
         Me.Controls.Add(Me.PB3)
         Me.Controls.Add(Me.PB1)
@@ -262,15 +303,17 @@ Partial Class Form1
         CType(Me.PB1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB15, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB14, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB13, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PB15, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB10, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB11, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB8, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB12, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PB9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.blankBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -283,12 +326,15 @@ Partial Class Form1
     Friend WithEvents PB1 As PictureBox
     Friend WithEvents PB3 As PictureBox
     Friend WithEvents PB2 As PictureBox
+    Friend WithEvents PB15 As PictureBox
     Friend WithEvents PB14 As PictureBox
     Friend WithEvents PB13 As PictureBox
-    Friend WithEvents PB15 As PictureBox
     Friend WithEvents PB10 As PictureBox
     Friend WithEvents PB11 As PictureBox
     Friend WithEvents PB8 As PictureBox
     Friend WithEvents PB12 As PictureBox
     Friend WithEvents PB9 As PictureBox
+    Friend WithEvents blankBox As PictureBox
+    Friend WithEvents congrats As Label
+    Friend WithEvents tip As Label
 End Class
--- CatPuzzle/WindowsApp1/WindowsApp1/Form1.vb
+++ CatPuzzle/WindowsApp1/WindowsApp1/Form1.vb
@@ -1,11 +1,159 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+    Private Sub load(sender As Object, e As EventArgs) Handles MyBase.Load
+        shuffle()
+    End Sub
 
+    Private Sub checkForWin()
+        Dim hasWon As Boolean
+        Dim pieces() As PictureBox
+        pieces = New PictureBox() {PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB14, PB15, PB13, blankBox}
+        hasWon = True
+
+        For Each piece As PictureBox In pieces
+            Select Case piece.Name
+                Case "PB1"
+                    If piece.Location.X <> 363 Or piece.Location.Y <> 27 Then
+                        hasWon = False
+                    End If
+                Case "PB2"
+                    If piece.Location.X <> 466 Or piece.Location.Y <> 27 Then
+                        hasWon = False
+                    End If
+                Case "PB3"
+                    If piece.Location.X <> 569 Or piece.Location.Y <> 27 Then
+                        hasWon = False
+                    End If
+                Case "PB4"
+                    If piece.Location.X <> 672 Or piece.Location.Y <> 27 Then
+                        hasWon = False
+                    End If
+                Case "PB5"
+                    If piece.Location.X <> 363 Or piece.Location.Y <> 180 Then
+                        hasWon = False
+                    End If
+                Case "PB6"
+                    If piece.Location.X <> 466 Or piece.Location.Y <> 180 Then
+                        hasWon = False
+                    End If
+                Case "PB7"
+                    If piece.Location.X <> 569 Or piece.Location.Y <> 180 Then
+                        hasWon = False
+                    End If
+                Case "PB8"
+                    If piece.Location.X <> 672 Or piece.Location.Y <> 180 Then
+                        hasWon = False
+                    End If
+                Case "PB9"
+                    If piece.Location.X <> 363 Or piece.Location.Y <> 333 Then
+                        hasWon = False
+                    End If
+                Case "PB10"
+                    If piece.Location.X <> 466 Or piece.Location.Y <> 333 Then
+                        hasWon = False
+                    End If
+                Case "PB11"
+                    If piece.Location.X <> 569 Or piece.Location.Y <> 333 Then
+                        hasWon = False
+                    End If
+                Case "PB12"
+                    If piece.Location.X <> 672 Or piece.Location.Y <> 333 Then
+                        hasWon = False
+                    End If
+                Case "PB13"
+                    If piece.Location.X <> 363 Or piece.Location.Y <> 486 Then
+                        hasWon = False
+                    End If
+                Case "PB14"
+                    If piece.Location.X <> 466 Or piece.Location.Y <> 486 Then
+                        hasWon = False
+                    End If
+                Case "PB15"
+                    If piece.Location.X <> 569 Or piece.Location.Y <> 486 Then
+                        hasWon = False
+                    End If
+            End Select
+        Next
+        If hasWon Then
+            congrats.Visible = True
+            tip.Visible = True
+        End If
+    End Sub
+
+    Private Sub shuffle()
+        Dim pieces() As PictureBox
+        Dim x, y, pieceHeight, pieceWidth As Integer
+        pieceHeight = 153
+        pieceWidth = 103
+        Dim taken(), newPoint As Point
+        Dim pointIsTaken As Boolean
+        pointIsTaken = True
+        congrats.Visible = False
+        tip.Visible = False
+        taken = New Point() {}
+        pieces = New PictureBox() {PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB14, PB15, PB13, blankBox}
+
+        Randomize()
+
+        For Each piece As PictureBox In pieces
+            While pointIsTaken
+                x = 363 + (Int((4 * Rnd())) * pieceWidth)
+                y = 27 + (Int((4 * Rnd())) * pieceHeight)
+                newPoint = New Point(x, y)
+                pointIsTaken = False
+                For Each point As Point In taken
+                    If point.X = newPoint.X And point.Y = newPoint.Y Then
+                        pointIsTaken = True
+                        Exit For
+                    End If
+                Next
+            End While
+            pointIsTaken = True
+            piece.Location = newPoint
+            Array.Resize(taken, taken.Length + 1)
+            taken(taken.Length - 1) = newPoint
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+        shuffle()
     End Sub
 
     Private Sub Puzzle_Click(sender As Object, e As EventArgs) Handles PB1.Click, PB2.Click, PB3.Click, PB4.Click, PB5.Click, PB6.Click, PB7.Click, PB8.Click, PB9.Click,
-            PB10.Click, PB11.Click, PB12.Click, PB13.Click, PB14.Click, PB15.Click
+            PB10.Click, PB11.Click, PB12.Click, PB14.Click, PB15.Click, PB13.Click
+        Dim pieceHeight, pieceWidth As Integer
+        pieceHeight = 153
+        pieceWidth = 103
+        Dim clickedPiece As PictureBox
+        clickedPiece = CType(sender, PictureBox)
 
+        If clickedPiece.Location.Y + pieceHeight = blankBox.Location.Y And clickedPiece.Location.X = blankBox.Location.X Then
+            Dim oldSpot As Point
+            oldSpot = clickedPiece.Location
+            clickedPiece.Location = New Point(clickedPiece.Location.X, clickedPiece.Location.Y + pieceHeight)
+            blankBox.Location = oldSpot
+            checkForWin()
 
+        ElseIf clickedPiece.Location.Y = blankBox.Location.Y + pieceHeight And clickedPiece.Location.X = blankBox.Location.X Then
+            Dim oldSpot As Point
+            oldSpot = clickedPiece.Location
+            clickedPiece.Location = New Point(clickedPiece.Location.X, clickedPiece.Location.Y - pieceHeight)
+            blankBox.Location = oldSpot
+            checkForWin()
+
+        ElseIf clickedPiece.Location.X = blankBox.Location.X + pieceWidth And clickedPiece.Location.Y = blankBox.Location.Y Then
+            Dim oldSpot As Point
+            oldSpot = clickedPiece.Location
+            clickedPiece.Location = New Point(clickedPiece.Location.X - pieceWidth, clickedPiece.Location.Y)
+            blankBox.Location = oldSpot
+            checkForWin()
+
+        ElseIf clickedPiece.Location.X + pieceWidth = blankBox.Location.X And clickedPiece.Location.Y = blankBox.Location.Y Then
+            Dim oldSpot As Point
+            oldSpot = clickedPiece.Location
+            clickedPiece.Location = New Point(clickedPiece.Location.X + pieceWidth, clickedPiece.Location.Y)
+            blankBox.Location = oldSpot
+            checkForWin()
+
+        End If
     End Sub
 End Class
