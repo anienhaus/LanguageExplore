820178f827cb5e7059955de452f9481eb975cdb2
Tracy Karol <tmkarol@mines.edu>
2018-11-27 14:42:45 -0700

    Started cat puzzle program

---
+++ CatPuzzle/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ResetButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PB4 = New System.Windows.Forms.PictureBox()
+        Me.PB5 = New System.Windows.Forms.PictureBox()
+        Me.PB6 = New System.Windows.Forms.PictureBox()
+        Me.PB7 = New System.Windows.Forms.PictureBox()
+        Me.PB1 = New System.Windows.Forms.PictureBox()
+        Me.PB3 = New System.Windows.Forms.PictureBox()
+        Me.PB2 = New System.Windows.Forms.PictureBox()
+        Me.PB14 = New System.Windows.Forms.PictureBox()
+        Me.PB13 = New System.Windows.Forms.PictureBox()
+        Me.PB15 = New System.Windows.Forms.PictureBox()
+        Me.PB10 = New System.Windows.Forms.PictureBox()
+        Me.PB11 = New System.Windows.Forms.PictureBox()
+        Me.PB8 = New System.Windows.Forms.PictureBox()
+        Me.PB12 = New System.Windows.Forms.PictureBox()
+        Me.PB9 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PB9, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ResetButton
+        '
+        Me.ResetButton.BackColor = System.Drawing.Color.White
+        Me.ResetButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ResetButton.Font = New System.Drawing.Font("Poor Richard", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResetButton.Location = New System.Drawing.Point(508, 680)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(121, 50)
+        Me.ResetButton.TabIndex = 0
+        Me.ResetButton.Text = "Reset"
+        Me.ResetButton.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.PictureBox1.Location = New System.Drawing.Point(353, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(447, 662)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PB4
+        '
+        Me.PB4.Image = CType(resources.GetObject("PB4.Image"), System.Drawing.Image)
+        Me.PB4.Location = New System.Drawing.Point(688, 27)
+        Me.PB4.Name = "PB4"
+        Me.PB4.Size = New System.Drawing.Size(103, 153)
+        Me.PB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB4.TabIndex = 2
+        Me.PB4.TabStop = False
+        '
+        'PB5
+        '
+        Me.PB5.Image = CType(resources.GetObject("PB5.Image"), System.Drawing.Image)
+        Me.PB5.Location = New System.Drawing.Point(363, 186)
+        Me.PB5.Name = "PB5"
+        Me.PB5.Size = New System.Drawing.Size(103, 153)
+        Me.PB5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB5.TabIndex = 3
+        Me.PB5.TabStop = False
+        '
+        'PB6
+        '
+        Me.PB6.Image = CType(resources.GetObject("PB6.Image"), System.Drawing.Image)
+        Me.PB6.Location = New System.Drawing.Point(472, 186)
+        Me.PB6.Name = "PB6"
+        Me.PB6.Size = New System.Drawing.Size(103, 153)
+        Me.PB6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB6.TabIndex = 4
+        Me.PB6.TabStop = False
+        '
+        'PB7
+        '
+        Me.PB7.Image = CType(resources.GetObject("PB7.Image"), System.Drawing.Image)
+        Me.PB7.Location = New System.Drawing.Point(579, 186)
+        Me.PB7.Name = "PB7"
+        Me.PB7.Size = New System.Drawing.Size(103, 153)
+        Me.PB7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB7.TabIndex = 5
+        Me.PB7.TabStop = False
+        '
+        'PB1
+        '
+        Me.PB1.Image = CType(resources.GetObject("PB1.Image"), System.Drawing.Image)
+        Me.PB1.Location = New System.Drawing.Point(363, 27)
+        Me.PB1.Name = "PB1"
+        Me.PB1.Size = New System.Drawing.Size(103, 153)
+        Me.PB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB1.TabIndex = 7
+        Me.PB1.TabStop = False
+        '
+        'PB3
+        '
+        Me.PB3.Image = CType(resources.GetObject("PB3.Image"), System.Drawing.Image)
+        Me.PB3.Location = New System.Drawing.Point(579, 27)
+        Me.PB3.Name = "PB3"
+        Me.PB3.Size = New System.Drawing.Size(103, 153)
+        Me.PB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB3.TabIndex = 8
+        Me.PB3.TabStop = False
+        '
+        'PB2
+        '
+        Me.PB2.Image = CType(resources.GetObject("PB2.Image"), System.Drawing.Image)
+        Me.PB2.Location = New System.Drawing.Point(470, 27)
+        Me.PB2.Name = "PB2"
+        Me.PB2.Size = New System.Drawing.Size(103, 153)
+        Me.PB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB2.TabIndex = 9
+        Me.PB2.TabStop = False
+        '
+        'PB14
+        '
+        Me.PB14.Image = CType(resources.GetObject("PB14.Image"), System.Drawing.Image)
+        Me.PB14.Location = New System.Drawing.Point(581, 504)
+        Me.PB14.Name = "PB14"
+        Me.PB14.Size = New System.Drawing.Size(103, 153)
+        Me.PB14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB14.TabIndex = 11
+        Me.PB14.TabStop = False
+        '
+        'PB13
+        '
+        Me.PB13.Image = CType(resources.GetObject("PB13.Image"), System.Drawing.Image)
+        Me.PB13.Location = New System.Drawing.Point(472, 504)
+        Me.PB13.Name = "PB13"
+        Me.PB13.Size = New System.Drawing.Size(103, 153)
+        Me.PB13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB13.TabIndex = 12
+        Me.PB13.TabStop = False
+        '
+        'PB15
+        '
+        Me.PB15.Image = CType(resources.GetObject("PB15.Image"), System.Drawing.Image)
+        Me.PB15.Location = New System.Drawing.Point(688, 504)
+        Me.PB15.Name = "PB15"
+        Me.PB15.Size = New System.Drawing.Size(103, 153)
+        Me.PB15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB15.TabIndex = 13
+        Me.PB15.TabStop = False
+        '
+        'PB10
+        '
+        Me.PB10.Image = CType(resources.GetObject("PB10.Image"), System.Drawing.Image)
+        Me.PB10.Location = New System.Drawing.Point(472, 345)
+        Me.PB10.Name = "PB10"
+        Me.PB10.Size = New System.Drawing.Size(103, 153)
+        Me.PB10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB10.TabIndex = 14
+        Me.PB10.TabStop = False
+        '
+        'PB11
+        '
+        Me.PB11.Image = CType(resources.GetObject("PB11.Image"), System.Drawing.Image)
+        Me.PB11.Location = New System.Drawing.Point(579, 345)
+        Me.PB11.Name = "PB11"
+        Me.PB11.Size = New System.Drawing.Size(103, 153)
+        Me.PB11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB11.TabIndex = 15
+        Me.PB11.TabStop = False
+        '
+        'PB8
+        '
+        Me.PB8.Image = CType(resources.GetObject("PB8.Image"), System.Drawing.Image)
+        Me.PB8.Location = New System.Drawing.Point(688, 186)
+        Me.PB8.Name = "PB8"
+        Me.PB8.Size = New System.Drawing.Size(103, 153)
+        Me.PB8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB8.TabIndex = 16
+        Me.PB8.TabStop = False
+        '
+        'PB12
+        '
+        Me.PB12.Image = CType(resources.GetObject("PB12.Image"), System.Drawing.Image)
+        Me.PB12.Location = New System.Drawing.Point(688, 345)
+        Me.PB12.Name = "PB12"
+        Me.PB12.Size = New System.Drawing.Size(103, 153)
+        Me.PB12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB12.TabIndex = 17
+        Me.PB12.TabStop = False
+        '
+        'PB9
+        '
+        Me.PB9.Image = CType(resources.GetObject("PB9.Image"), System.Drawing.Image)
+        Me.PB9.Location = New System.Drawing.Point(363, 345)
+        Me.PB9.Name = "PB9"
+        Me.PB9.Size = New System.Drawing.Size(103, 153)
+        Me.PB9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PB9.TabIndex = 18
+        Me.PB9.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1188, 795)
+        Me.Controls.Add(Me.PB9)
+        Me.Controls.Add(Me.PB12)
+        Me.Controls.Add(Me.PB8)
+        Me.Controls.Add(Me.PB11)
+        Me.Controls.Add(Me.PB10)
+        Me.Controls.Add(Me.PB15)
+        Me.Controls.Add(Me.PB13)
+        Me.Controls.Add(Me.PB14)
+        Me.Controls.Add(Me.PB2)
+        Me.Controls.Add(Me.PB3)
+        Me.Controls.Add(Me.PB1)
+        Me.Controls.Add(Me.PB7)
+        Me.Controls.Add(Me.PB6)
+        Me.Controls.Add(Me.PB5)
+        Me.Controls.Add(Me.PB4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ResetButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PB9, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ResetButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PB4 As PictureBox
+    Friend WithEvents PB5 As PictureBox
+    Friend WithEvents PB6 As PictureBox
+    Friend WithEvents PB7 As PictureBox
+    Friend WithEvents PB1 As PictureBox
+    Friend WithEvents PB3 As PictureBox
+    Friend WithEvents PB2 As PictureBox
+    Friend WithEvents PB14 As PictureBox
+    Friend WithEvents PB13 As PictureBox
+    Friend WithEvents PB15 As PictureBox
+    Friend WithEvents PB10 As PictureBox
+    Friend WithEvents PB11 As PictureBox
+    Friend WithEvents PB8 As PictureBox
+    Friend WithEvents PB12 As PictureBox
+    Friend WithEvents PB9 As PictureBox
+End Class
---
+++ CatPuzzle/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+
+    End Sub
+
+    Private Sub Puzzle_Click(sender As Object, e As EventArgs) Handles PB1.Click, PB2.Click, PB3.Click, PB4.Click, PB5.Click, PB6.Click, PB7.Click, PB8.Click, PB9.Click,
+            PB10.Click, PB11.Click, PB12.Click, PB13.Click, PB14.Click, PB15.Click
+
+
+    End Sub
+End Class
