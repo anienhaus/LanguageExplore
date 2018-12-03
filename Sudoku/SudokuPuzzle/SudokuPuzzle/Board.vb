e3c8570155cf18358d6231fc32fceed46dbd22a3
Clara <claratran@mymail.mines.edu>
2018-12-02 17:59:09 -0700

    Created Sudoku project, added board

---
+++ Sudoku/SudokuPuzzle/SudokuPuzzle/Board.vb
@@ -0,0 +1,33 @@
+﻿Public Class Board
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles Cell1_1.TextChanged
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub RichTextBox2_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox2.TextChanged
+
+    End Sub
+
+    Private Sub RichTextBox4_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox4.TextChanged
+
+    End Sub
+
+    Private Sub RichTextBox18_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox18.TextChanged
+
+    End Sub
+
+    Private Sub RichTextBox54_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
